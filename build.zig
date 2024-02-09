edc6a5df2642fdd614d5912faa30ba3f47256a2d
paoda <rekai@musuka.dev>
2024-02-08 22:39:12 -0600

    chore: update to 0.12.0-dev.2063+804cee3b9

--- build.zig
+++ build.zig
@@ -1,47 +1,20 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) !void {
-    var target = b.standardTargetOptions(.{});
+    const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const nfd = b.addModule("nfd", .{ .source_file = .{ .path = "src/lib.zig" } });
+    const mod = b.addModule("nfd", .{ .root_source_file = .{ .path = "src/lib.zig" } });
 
-    const lib = b.addStaticLibrary(.{
+    const lib = createStaticLibrary(b, .{
         .name = "nfd",
         .root_source_file = .{ .path = "src/lib.zig" },
-        .main_mod_path = .{ .path = "." },
         .target = target,
         .optimize = optimize,
+        .link_libc = true,
     });
-    lib.addModule("nfd", nfd);
 
-    const cflags = [_][]const u8{"-Wall"};
-    lib.addIncludePath(.{ .path = "nativefiledialog/src/include" });
-    lib.addCSourceFile(.{ .file = .{ .path = "nativefiledialog/src/nfd_common.c" }, .flags = &cflags });
-    if (lib.target.isDarwin()) {
-        lib.addCSourceFile(.{ .file = .{ .path = "nativefiledialog/src/nfd_cocoa.m" }, .flags = &cflags });
-    } else if (lib.target.isWindows()) {
-        lib.addCSourceFile(.{ .file = .{ .path = "nativefiledialog/src/nfd_win.cpp" }, .flags = &cflags });
-    } else {
-        lib.addCSourceFile(.{ .file = .{ .path = "nativefiledialog/src/nfd_gtk.c" }, .flags = &cflags });
-    }
-
-    lib.linkLibC();
-    if (lib.target.isDarwin()) {
-        lib.linkFramework("AppKit");
-    } else if (lib.target.isWindows()) {
-        lib.linkSystemLibrary("shell32");
-        lib.linkSystemLibrary("ole32");
-        lib.linkSystemLibrary("uuid"); // needed by MinGW
-    } else {
-        lib.linkSystemLibrary("atk-1.0");
-        lib.linkSystemLibrary("gdk-3");
-        lib.linkSystemLibrary("gtk-3");
-        lib.linkSystemLibrary("glib-2.0");
-        lib.linkSystemLibrary("gobject-2.0");
-    }
-    lib.installHeadersDirectory("nativefiledialog/src/include", ".");
-    b.installArtifact(lib);
+    mod.linkLibrary(lib);
 
     var demo = b.addExecutable(.{
         .name = "nfd-demo",
@@ -49,9 +22,8 @@ pub fn build(b: *std.Build) !void {
         .target = target,
         .optimize = optimize,
     });
-    demo.addIncludePath(.{ .path = "nativefiledialog/src/include" });
-    demo.addModule("nfd", nfd);
-    demo.linkLibrary(lib);
+
+    demo.root_module.addImport("nfd", mod);
     b.installArtifact(demo);
 
     const run_demo_cmd = b.addRunArtifact(demo);
@@ -59,4 +31,39 @@ pub fn build(b: *std.Build) !void {
 
     const run_demo_step = b.step("run", "Run the demo");
     run_demo_step.dependOn(&run_demo_cmd.step);
+}
+
+fn createStaticLibrary(b: *std.Build, options: std.Build.StaticLibraryOptions) *std.Build.Step.Compile {
+    const lib = b.addStaticLibrary(options);
+
+    const cflags = [_][]const u8{"-Wall"};
+    lib.addIncludePath(.{ .path = "nativefiledialog/src/include" });
+    lib.addCSourceFile(.{ .file = .{ .path = "nativefiledialog/src/nfd_common.c" }, .flags = &cflags });
+
+    switch (options.target.result.os.tag) {
+        .macos => lib.addCSourceFile(.{ .file = .{ .path = "nativefiledialog/src/nfd_cocoa.m" }, .flags = &cflags }),
+        .windows => lib.addCSourceFile(.{ .file = .{ .path = "nativefiledialog/src/nfd_win.cpp" }, .flags = &cflags }),
+        .linux => lib.addCSourceFile(.{ .file = .{ .path = "nativefiledialog/src/nfd_gtk.c" }, .flags = &cflags }),
+        else => @panic("unsupported OS "),
+    }
+
+    switch (options.target.result.os.tag) {
+        .macos => lib.linkFramework("AppKit"),
+        .windows => {
+            lib.linkSystemLibrary("shell32");
+            lib.linkSystemLibrary("ole32");
+            lib.linkSystemLibrary("uuid"); // needed by MinGW
+        },
+        .linux => {
+            lib.linkSystemLibrary("atk-1.0");
+            lib.linkSystemLibrary("gdk-3");
+            lib.linkSystemLibrary("gtk-3");
+            lib.linkSystemLibrary("glib-2.0");
+            lib.linkSystemLibrary("gobject-2.0");
+        },
+        else => @panic("unsupported OS"),
+    }
+    lib.installHeadersDirectory("nativefiledialog/src/include", ".");
+
+    return lib;
 }
